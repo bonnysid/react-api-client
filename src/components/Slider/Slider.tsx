98e8e113797511ab79e02ea1d245dda7f8e9a4cc
bonnysid <bonnysidworker@gmail.com>
2021-04-25 22:52:15 +0300

    feat: create Slider

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,80 @@
+import React, {FC, useRef} from "react";
+import s from './Slider.module.css'
+
+
+const Slider: FC = ({children}) => {
+    const slider = useRef<HTMLDivElement | null>(null)
+    const startX = useRef(0)
+    const isDown = useRef(false)
+    const translate = useRef(0)
+    const offsetLeft = useRef(0)
+
+    const onSliderMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        isDown.current = true
+        startX.current = e.pageX - slider.current!.offsetLeft
+    }
+
+    const isRightSliderEnd = () => {
+        if (translate.current < Math.round(-slider.current!.offsetWidth / 2 - 20)) {
+            translate.current = Math.round(-slider.current!.offsetWidth / 2 - 20)
+            slider.current!.style.transform = `translate(${translate.current}px)`
+            return true
+        }
+        return false
+    }
+
+    const isLeftSliderEnd = () => {
+        if (translate.current > 0) {
+            translate.current = 0
+            slider.current!.style.transform = `translate(${translate.current}px)`
+            return true
+        }
+        return false
+    }
+
+    const onSliderMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (!isDown.current) return
+        e.preventDefault()
+        const x = e.pageX - offsetLeft.current
+        const walk = x - startX.current
+        translate.current = Math.sign(walk) === -1 ? translate.current - 5 : translate.current + 5
+
+        if (isRightSliderEnd()) return
+        else if(isLeftSliderEnd()) return
+
+        slider.current!.style.transform = `translate(${translate.current}px)`
+    }
+
+    const onSliderMouseUp = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        isDown.current = false
+    }
+
+    const onSliderMouseLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        isDown.current = false
+    }
+
+    const onSliderScroll = (e: React.WheelEvent<HTMLDivElement>) => {
+        const walk = e.deltaY
+        translate.current = Math.sign(walk) === -1 ? translate.current - 20 : translate.current + 20
+
+        if (isRightSliderEnd()) return
+        else if(isLeftSliderEnd()) return
+
+        slider.current!.style.transform = `translate(${translate.current}px)`
+    }
+
+    return (
+            <div
+                ref={slider}
+                onMouseDown={onSliderMouseDown}
+                onMouseLeave={onSliderMouseLeave}
+                onMouseMove={onSliderMouseMove}
+                onMouseUp={onSliderMouseUp}
+                onWheel={onSliderScroll}
+                className={s.items}>
+                {children}
+            </div>
+    )
+}
+
+export default Slider
--- src/containers/RequestHistory/RequestHistory.tsx
+++ src/containers/RequestHistory/RequestHistory.tsx
@@ -6,6 +6,7 @@ import RequestHistoryItem from "./RequestHistoryItem/RequestHistoryItem";
 import {useActions} from "../../hooks/useActions";
 import {QuerySendsay} from "../../types/types";
 import ScrollMenu from "react-horizontal-scrolling-menu";
+import Slider from "../../components/Slider/Slider";
 
 export interface RequestHistoryProps {
     execQuery: (value: QuerySendsay) => void
@@ -14,67 +15,17 @@ export interface RequestHistoryProps {
 const RequestHistory: FC<RequestHistoryProps> = ({execQuery}) => {
     const {history, loading, response} = useTypedSelector(state => state.console)
     const {clearAllQueries} = useActions()
-    const slider = useRef<HTMLDivElement | null>(null)
-    const startX = useRef(0)
-    const isDown = useRef(false)
-    const translate = useRef(0)
-    const offsetLeft = useRef(0)
 
-    const historyElements = [...history.items].reverse().map(item => <RequestHistoryItem execQuery={execQuery} key={item.id} item={item}/>)
-
-    const onSliderMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        isDown.current = true
-        startX.current = e.pageX - slider.current!.offsetLeft
-    }
-
-    const onSliderMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        if(!isDown.current) return
-        e.preventDefault()
-        const x = e.pageX - offsetLeft.current
-        const walk = x - startX.current
-        console.log(`${translate.current} + ${walk} = ${translate.current + walk}`)
-        translate.current = Math.sign(walk) === -1 ? translate.current - 5 : translate.current + 5
-        if(translate.current < Math.round(-slider.current!.offsetWidth / 2)) {
-            translate.current = Math.round(-slider.current!.offsetWidth / 2)
-            slider.current!.style.transform = `translate(${translate.current}px)`
-            return
-        }
-        else if (translate.current > 0) {
-            translate.current = 0
-            slider.current!.style.transform = `translate(${translate.current}px)`
-            return
-        }
-        slider.current!.style.transform = `translate(${translate.current}px)`
-    }
-
-    const onSliderMouseUp = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        isDown.current = false
-    }
-
-    const onSliderMouseLeave = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        isDown.current = false
-    }
-
-    const onSliderScroll = (e: React.WheelEvent<HTMLDivElement>) => {
-    }
+    const historyElements = [...history.items].reverse().map(item => <RequestHistoryItem execQuery={execQuery}
+                                                                                         key={item.id} item={item}/>)
 
     return (
         <div className={s.container}>
-            {/*<ScrollMenu*/}
-            {/*    data={historyElements}*/}
-            {/*    dragging={true}*/}
-            {/*/>*/}
-            <div
-                ref={slider}
-                onMouseDown={onSliderMouseDown}
-                onMouseLeave={onSliderMouseLeave}
-                onMouseMove={onSliderMouseMove}
-                onMouseUp={onSliderMouseUp}
-                onWheel={onSliderScroll}
-                className={s.items}>
+            <Slider>
                 {historyElements}
-            </div>
-            <button onClick={clearAllQueries} className={s.deleteBtn}><SvgIcon className={s.icon} urlId={'cross'}/></button>
+            </Slider>
+            <button onClick={clearAllQueries} className={s.deleteBtn}><SvgIcon className={s.icon} urlId={'cross'}/>
+            </button>
         </div>
     )
 }
