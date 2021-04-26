3db4b95dda140d4745d2f4fe49f9174a65307e4d
bonnysid <bonnysidworker@gmail.com>
2021-04-26 12:12:50 +0300

    feat: add ModalPortal

---
+++ src/containers/ModalPortal/ModalPortal.tsx
@@ -0,0 +1,27 @@
+import {FC, useEffect} from "react";
+import ReactDOM from "react-dom";
+
+export interface ModalPortalProps {
+    el?: string
+    className?: string
+}
+
+const ModalPortal: FC<ModalPortalProps> = ({
+                                               children,
+                                               el = 'div',
+                                               className = 'root-modal'
+                                           }) => {
+    const root = document.createElement(el)
+    root.classList.add(className)
+
+    useEffect(() => {
+        document.body.appendChild(root)
+        return () => {
+            document.body.removeChild(root)
+        }
+    }, [])
+
+    return ReactDOM.createPortal(children, root)
+}
+
+export default ModalPortal
